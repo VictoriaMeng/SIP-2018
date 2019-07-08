f4253ced2d20960d0cd5459ff86a667507de1396
Victoria Meng <vym205@nyu.edu>
2019-07-07 21:54:25 -0500

    Add Python Practice Problems

---
+++ victoria/python_practice.py
@@ -0,0 +1,65 @@
+# Python Practice Problems
+
+# Don't forget to print() your answers to the console!
+# Google or check Python documentation for functions you don't remember!
+
+
+# Strings:
+
+# 1. Given the string 'hello', give an index command that returns 'e'.
+# 2. Then, give two methods of retrieving the letter 'o' from 'hello' using indexing.
+s = 'hello'
+
+
+# Lists:
+
+# 1. Reassign 'hello' in this nested list to say 'goodbye' instead
+list3 = [1,2,[3,4,'hello']]
+
+# 2. Sort the list below
+list4 = [5,3,4,6,1]
+
+
+# Tuples:
+# What is the major difference between tuples and lists?
+# (Type answer here.)
+
+# How do you create a tuple?
+# (Type answer here.)
+
+
+# Statements
+# 1. Use for, .split(), and if to create a Statement that will print out words that start with 's':
+st1 = 'Print only the words that start with s in this sentence'
+
+# 2. Use range() to print all the even numbers from 0 to 10.
+
+# 3. Go through the string below and if the length of a word is even print "even!"
+st2 = 'Print even for every word in this sentence that has an even number of letters'
+
+# 4. Write a program that prints the integers from 1 to 100:
+#   - For multiples of three, print "Fizz" instead of the number
+#   - For multiples of five, print "Buzz" instead of the number
+#   - For numbers which are multiples of both three and five, print "FizzBuzz" instead of the number.
+
+
+# Functions (Warmup):
+# 1. LESSER OF TWO EVENS: Write a function that:
+#   -Returns the lesser of two given numbers if both numbers are even
+#   -But returns the greater if one or both numbers are odd
+
+# 2. Write a function that:
+#   -Takes a two-word string and returns True if both words begin with same letter
+
+# 3. MAKES TWENTY: Write a function that:
+#   -Given two integers, return True if the sum of the integers is 20 OR if one of the integers is 20.
+#   -If not, return False
+
+# Functions (Level 1):
+# 1. Write a function that capitalizes the first and fourth letters of a name
+#   -HINT: If you're stuck, look up the String.replace() and capitalize() functions
+#   -HINT: String.replace() doesn't change the original string, but variables can always be reassigned...
+
+# 2. Write a function that given a sentence, returns with the words reversed
+#   -HINT: The split() and join() functions may be useful here
+#   -string_name[::-1] is a commonly used syntax to reverse Strings
