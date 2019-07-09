9b8ec249fc1b68d2b94842f83b2ded4a81a498dd
Victoria Meng <vym205@nyu.edu>
2019-07-09 08:56:34 -0500

    Refactor survey_project into master run() function

--- U2-Applications/U2.1-Data/SurveyProject/solutions-victoria/survey_project.py
+++ U2-Applications/U2.1-Data/SurveyProject/solutions-victoria/survey_project.py
@@ -15,10 +15,10 @@ questions = {
 def show_question(q):
     print(f"{questions[q]}: ")
 
-def get_answer(q):
+def get_answer(q, survey):
     survey[q] = input()
 
-def save_answer():
+def save_answer(survey):
     answers.append(survey)
 
 def show_quit_prompt():
@@ -40,20 +40,16 @@ def save_to_json():
             index += 1
         f.write(']')
 
-while True:
-    survey = {}
-    for q in questions:
-        show_question(q)
-        get_answer(q)
-    save_answer()
-    show_quit_prompt()
-    if quit_survey():
-        break
-        
-save_to_json()
+def run():
+    while True:
+        survey = {}
+        for q in questions:
+            show_question(q)
+            get_answer(q, survey)
+        save_answer(survey)
+        show_quit_prompt()
+        if quit_survey():
+            break
+    save_to_json()
 
-
-#
-# f = open("survey_answers.json", "w")
-#
-# f.close()
+run()
