0768050dc39cfce7eecfa339e4e306c9dfa44bb4
VictoriaMeng <vym205@protonmail.com>
2019-07-10 08:50:56 -0500

    Print questions in input() instead of separate function

--- U2-Applications/U2.1-Data/SurveyProject/solutions-victoria/survey_project.py
+++ U2-Applications/U2.1-Data/SurveyProject/solutions-victoria/survey_project.py
@@ -12,13 +12,10 @@ questions = {
             "favorite app": "Name the app/program/site you use most often"
             }
 
-def show_question(q):
-    print(f"{questions[q]}: ")
-
 def get_answer(q, survey):
-    survey[q] = input()
+    survey[q] = input(f"{questions[q]}: ")
 
-def save_answer(survey):
+def append_answer(survey):
     answers.append(survey)
 
 def show_quit_prompt():
@@ -44,9 +41,8 @@ def run():
     while True:
         survey = {}
         for q in questions:
-            show_question(q)
             get_answer(q, survey)
-        save_answer(survey)
+            append_answer(survey)
         show_quit_prompt()
         if quit_survey():
             break
